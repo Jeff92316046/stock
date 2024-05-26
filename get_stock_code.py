a4c5db6f2e7c48283e4de6ab2e7112014b3396af
JEFF Liao <jeffliao92316046@gmail.com>
2024-05-26 22:31:02 +0800

    refactor

--- get_stock_code.py
+++ get_stock_code.py
@@ -1,109 +1,80 @@
+import time
 from selenium import webdriver
 import sqlite3
 
 
+con = sqlite3.connect('database.db')
+cur = con.cursor()
 
 def create_table():
-    con = sqlite3.connect('stock_code_list.db')
-    cur = con.cursor()
-    cur.execute("CREATE TABLE data (stock text)")
+    cur.execute("CREATE TABLE IF NOT EXISTS stock_list (stock text unique,do_flag integer)")
     con.commit()
     con.close()
 
 def insert_data(stock):
-    con = sqlite3.connect('stock_code_list.db')
-    cur = con.cursor()
 
-    cur.execute("INSERT INTO data VALUES (" + "'" + str(stock) + "')")
-
-    con.commit()
-    con.close()
+    cur.execute(f"INSERT OR IGNORE INTO stock_list VALUES ('{str(stock)}','0')")
+    
+    
 
 def get_stock_code_data():
     stock_code_list = []
-    con = sqlite3.connect('stock_code_list.db')
-    cur = con.cursor()
-
-    cur.execute("select * from data")
+    cur.execute("select * from stock_list where do_flag = 0")
     for row in cur:
         stock_code_list.append(row[0])
-    con.commit()
     con.close()
+    print(stock_code_list)
     return stock_code_list
 
-def del_stock_code(stock):
-    con = sqlite3.connect('stock_code_list.db')
-    cur = con.cursor()
-    cur.execute("DELETE FROM data WHERE stock = " + str(stock))
+def set_stock_code(stock):
+    cur.execute(f"UPDATE stock_list SET do_flag = 1 WHERE stock = '{stock}'")
     con.commit()
     con.close()
 
-def del_all_stock_code():
-    con = sqlite3.connect('stock_code_list.db')
-    cur = con.cursor()
-    cur.execute("DELETE FROM data")
+def refresh_all_stock_code():
+    cur.execute("UPDATE stock_list SET do_flag = 0")
     con.commit()
     con.close()
 
-def scrapying_1():
-    print("開始下載股票代碼清單")
-    del_all_stock_code()#clear db
+def scrapying_stock_list():
+    #del_all_stock_code()#clear db
+    refresh_all_stock_code()
     options = webdriver.ChromeOptions()
     options.add_argument('--headless')
-
     driver=webdriver.Chrome(options=options)
     #driver=webdriver.Chrome()
     driver.get("https://www.tdcc.com.tw/portal/zh/smWeb/psi")
-
-    button = driver.find_element('xpath','/html/body/div[1]/div[1]/div/main/div[4]/form/table/tbody/tr[6]/td/input')
-    button.click()
-
     
-
-    date_list_str = driver.find_element('xpath',"/html/body/div[1]/div[1]/div/main/div[6]/div/table/tbody")    #"tr[1""]"
-
-    
-    stock_code = date_list_str.find_elements('xpath',"./*")
-
-    for i in stock_code:
-        print(i.find_element('xpath',"./td[1]").text + " " + str(len(i.find_element('xpath',"./td[1]").text)))
-        if len(i.find_element('xpath',"./td[1]").text) == 4:
-            insert_data(i.find_element('xpath',"./td[1]").text)
-        
-    #for i in stock_code:
-    #    print(i.find_element('xpath',"./td[1]").text)
-def scrapying_2():
-    
-    options = webdriver.ChromeOptions()
-    options.add_argument('--headless')
-    
-    driver=webdriver.Chrome(options=options)
-    #driver=webdriver.Chrome()
     try:
-        driver.get("https://www.tdcc.com.tw/portal/zh/smWeb/psi")
-
-        option = driver.find_element('xpath',"/html/body/div[1]/div[1]/div/main/div[4]/form/table/tbody/tr[1]/td[2]/select/option[2]")
-        option.click()
+        """ start = time.time() """
         button = driver.find_element('xpath','/html/body/div[1]/div[1]/div/main/div[4]/form/table/tbody/tr[6]/td/input')
         button.click()
-
-        
-
         date_list_str = driver.find_element('xpath',"/html/body/div[1]/div[1]/div/main/div[6]/div/table/tbody")    #"tr[1""]"
-
-        
         stock_code = date_list_str.find_elements('xpath',"./*")
-
         for i in stock_code:
             print(i.find_element('xpath',"./td[1]").text + " " + str(len(i.find_element('xpath',"./td[1]").text)))
             if len(i.find_element('xpath',"./td[1]").text) == 4:
                 insert_data(i.find_element('xpath',"./td[1]").text)
+        option = driver.find_element('xpath',"/html/body/div[1]/div[1]/div/main/div[4]/form/table/tbody/tr[1]/td[2]/select/option[2]")
+        option.click()
+        button = driver.find_element('xpath','/html/body/div[1]/div[1]/div/main/div[4]/form/table/tbody/tr[6]/td/input')
+        button.click()
+        date_list_str = driver.find_element('xpath',"/html/body/div[1]/div[1]/div/main/div[6]/div/table/tbody")    #"tr[1""]"
+        stock_code = date_list_str.find_elements('xpath',"./*")
+        for i in stock_code:
+            print(i.find_element('xpath',"./td[1]").text + " " + str(len(i.find_element('xpath',"./td[1]").text)))
+            if len(i.find_element('xpath',"./td[1]").text) == 4:
+                insert_data(i.find_element('xpath',"./td[1]").text)
+        con.commit()
+        con.close()
+        """ end = time.time()
+        print(end-start) """
     except:
-        
-        pass
-    print("股票代碼清單下載完畢")
-
-
+        pass    
+    #for i in stock_code:
+    #    print(i.find_element('xpath',"./td[1]").text)
+""" if __name__ == '__main__':
+    scrapying_stock_list() """
 '''
 for i in stock_code:
     print(i)
--- scrapying.py
+++ scrapying.py
@@ -6,7 +6,8 @@ import logging
 
 people = []
 shares = []
-
+con = sqlite3.connect('database.db')
+cur = con.cursor()
 #date_start = 0
 
 def clean_str(a):
@@ -16,6 +17,8 @@ def clean_str(a):
 def scrapying_2(week):
     #create_table()
     #get_stock_code.create_table()
+    get_stock_code.refresh_all_stock_code()
+    get_stock_code.scrapying_stock_list()
     logging.basicConfig(level=logging.INFO,filename='std.log', filemode='w', format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',encoding="utf-8")
     logging.info("開始下載股票資訊")
     options = webdriver.ChromeOptions()
@@ -26,101 +29,80 @@ def scrapying_2(week):
     driver.get("https://www.tdcc.com.tw/portal/zh/smWeb/qryStock")
     """ get_stock_code.scrapying_1()
     get_stock_code.scrapying_2() """
-    gsc = get_stock_code.get_stock_code_data()
-
     
-    for k in range(0,len(gsc)):  
-    #for k in range(1):  
-        #try:
-        code = driver.find_element('xpath','/html/body/div[1]/div[1]/div/main/div[4]/form/table/tbody/tr[2]/td[2]/input')
-        code.clear()
-        code.send_keys(gsc[k])
-        logging.info(f"{gsc[k]}")
-        time.sleep(0.2)
-        date_list_str = "/html/body/div[1]/div[1]/div/main/div[4]/form/table/tbody/tr[1]/td[2]/select/option"
-        
-        for j in range(0,week) :
-            #try:
-            date_option = driver.find_element('xpath',f"{date_list_str}[{str(j+1)}]")
-            date_text = date_option.text
-            date_option.click()
-            time.sleep(0.3)
-            #if j == 0:
-            #    date = clean_str(date_option.text)
-            click1 = driver.find_element('xpath','/html/body/div[1]/div[1]/div/main/div[4]/form/table/tbody/tr[4]/td/input')
-            click1.click()
-            time.sleep(0.4)
-            c_counter = 0
-            while c_counter<16:
-                temp_1 = driver.find_elements('xpath',"/html/body/div[1]/div[1]/div/main/div[6]/div/table/tbody/tr["+str(c_counter+1)+"]/td[3]")
-                temp_2 = driver.find_elements('xpath',"/html/body/div[1]/div[1]/div/main/div[6]/div/table/tbody/tr["+str(c_counter+1)+"]/td[4]")
-                if len(temp_1) != 0 and len(temp_2)!=0 :
-                    insert_data(clean_str(gsc[k]),clean_str(date_text),clean_str(c_counter+1),clean_str(temp_1[0].text),clean_str(temp_2[0].text))
-                    c_counter += 1
-                else:
-                    temp_3 = driver.find_element('xpath','/html/body/div[1]/div[1]/div/main/div[6]/div/table/tbody/tr/td/span').text
-                    if temp_3 == '查無此資料':
-                        logging.warning(f"stock :1295,date :{clean_str(date_text)} || no data")
-                        break
-                    c_counter -= 1
-                   # print("c")
-                
-            #print ("S"+str(j+1))
-            """ except:
-                print("b") """
-            #except:                       
-            #   check = driver.find_element('xpath',"/html/body/div[1]/div[1]/div/main/div[6]/div/table/tbody/tr/td").text  
-            #   print(check)
-        get_stock_code.del_stock_code(gsc[k])
-        time.sleep(0.1)
-        """ except:
-            print("a") """
+    #gsc = ['0050','0051']
+    while(True):
+        gsc = get_stock_code.get_stock_code_data()
+        if len(gsc) == 0:
+            break
+        try :
+            for k in range(0,len(gsc)):  
+                code = driver.find_element('xpath','/html/body/div[1]/div[1]/div/main/div[4]/form/table/tbody/tr[2]/td[2]/input')
+                code.clear()
+                code.send_keys(gsc[k])
+                logging.info(f"{gsc[k]}")
+                time.sleep(0.2)
+                date_list_str = "/html/body/div[1]/div[1]/div/main/div[4]/form/table/tbody/tr[1]/td[2]/select/option"
+                for j in range(0,week) :
+                    #try:
+                    date_option = driver.find_element('xpath',f"{date_list_str}[{str(j+1)}]")
+                    date_text = date_option.text
+                    date_option.click()
+                    time.sleep(0.3)
+                    #if j == 0:
+                    #    date = clean_str(date_option.text)
+                    click1 = driver.find_element('xpath','/html/body/div[1]/div[1]/div/main/div[4]/form/table/tbody/tr[4]/td/input')
+                    click1.click()
+                    time.sleep(0.4)
+                    c_counter = 0
+                    while c_counter<16:
+                        temp_1 = driver.find_elements('xpath',"/html/body/div[1]/div[1]/div/main/div[6]/div/table/tbody/tr["+str(c_counter+1)+"]/td[3]")
+                        temp_2 = driver.find_elements('xpath',"/html/body/div[1]/div[1]/div/main/div[6]/div/table/tbody/tr["+str(c_counter+1)+"]/td[4]")
+                        if len(temp_1) != 0 and len(temp_2)!=0 :
+                            insert_data(clean_str(gsc[k]),clean_str(date_text),clean_str(c_counter+1),clean_str(temp_1[0].text),clean_str(temp_2[0].text))
+                            c_counter += 1
+                        else:
+                            temp_3 = driver.find_element('xpath','/html/body/div[1]/div[1]/div/main/div[6]/div/table/tbody/tr/td/span').text
+                            if temp_3 == '查無此資料':
+                                logging.warning(f"stock :1295,date :{clean_str(date_text)} || no data")
+                                break
+                            c_counter -= 1
+                get_stock_code.set_stock_code(gsc[k])
+                time.sleep(0.1)
+            con.commit()
+        except :
+            con.commit()
+            pass
     logging.info("股票資訊下載完畢")
-    #print("可以執行其他程序")
+    con.close()
 
 
 def create_table():
-    con = sqlite3.connect('database.db')
-    cur = con.cursor()
-    cur.execute("CREATE TABLE data (id INTEGER PRIMARY KEY AUTOINCREMENT,stock text , date_time text , number integer , people integer , share integer )")
+    cur.execute("CREATE TABLE IF NOT EXISTS data (id INTEGER PRIMARY KEY AUTOINCREMENT,stock text , date_time text , number integer , people integer , share integer )")
     con.commit()
     con.close()
 
-def del_stock_code(stock):
-    con = sqlite3.connect('database.db')
-    cur = con.cursor()
-    cur.execute("DELETE FROM data WHERE stock = '" + stock + "'" )
-    con.commit()
-    con.close()
     
 def insert_data(stock ,date ,number ,people ,share):
-    con = sqlite3.connect('database.db')
-    cur = con.cursor()
-    print(f"Select * from data where "
-            f"stock='{stock}' and "
-            f"date_time='{date}' and "
-            f"number ='{number}'")
-    logging.info(f"stock :{stock}, date :{date}, number :{number} || has exist")
-    cur.execute(f"Select * from data where "
-            f"stock='{stock}' and "
-            f"date_time='{date}' and "
-            f"number ='{number}'")
-   # print(cur.fetchall())
-    if(len(cur.fetchall()) == 0):
-        """ print(f"INSERT INTO data(stock,date_time,number,people,share) VALUES" 
-                    f"('{stock}'," 
-                    f"'{date}',"
-                    f"'{number}',"
-                    f"'{people}',"
-                    f"'{share}')") """
-        logging.info(f"stock :{stock}, date :{date}, number :{number}")
-        cur.execute(f"INSERT INTO data(stock,date_time,number,people,share) VALUES" 
-                    f"('{stock}'," 
-                    f"'{date}',"
-                    f"'{number}',"
-                    f"'{people}',"
-                    f"'{share}')")
-    con.commit()
-    con.close()
+    #用?
+    print(f"INSERT INTO data(stock,date_time,number,people,share) VALUES" 
+                f"('{stock}'," 
+                f"'{date}',"
+                f"'{number}',"
+                f"'{people}',"
+                f"'{share}')")
+    logging.info(f"stock :{stock}, date :{date}, number :{number}")
+    cur.execute(f"INSERT INTO data(id,stock,date_time,number,people,share) SELECT " 
+                f"NULL,"
+                f"'{stock}'," 
+                f"'{date}',"
+                f"'{number}',"
+                f"'{people}',"
+                f"'{share}' "
+                f"WHERE NOT EXISTS(SELECT * FROM data WHERE "
+                f"stock='{stock}' and "
+                f"date_time='{date}' and "
+                f"number='{number}')")
+    
 if __name__ == '__main__':
-    scrapying_2(53)
+    scrapying_2(1)
