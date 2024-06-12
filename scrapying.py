8bd53428aa20ce5e60d40ed0ee5f6dabcfe2ff76
JEFF Liao <jeffliao92316046@gmail.com>
2024-06-12 21:45:18 +0800

    owo

--- scrapying.py
+++ scrapying.py
@@ -1,4 +1,5 @@
 from selenium import webdriver
+from selenium.webdriver.chrome.service import Service as ChromeService
 import time
 import get_stock_code
 import sqlite3
@@ -6,7 +7,7 @@ import logging
 
 people = []
 shares = []
-
+t = time.time()
 #date_start = 0
 
 def clean_str(a):
@@ -14,17 +15,17 @@ def clean_str(a):
 
 
 def scrapying_2(week):
-    
-    logging.basicConfig(level=logging.INFO,filename='std.log', filemode='w', format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',encoding="utf-8")
-    logging.info("start execute program")
+    now_time = time.localtime(t)
+    file_time_str = time.strftime('%Y%m%d%H%M%S',now_time)
+    print("start execute program")
     create_table()
     get_stock_code.create_table()
-    logging.info("scrapying stock list")
-    #get_stock_code.scrapying_stock_list()
-    
+    print("scrapying stock list")
+    get_stock_code.scrapying_stock_list()
+
     """ get_stock_code.scrapying_1()
     get_stock_code.scrapying_2() """
-    
+
     #gsc = ['0050','0051']
     while(True):
         options = webdriver.ChromeOptions()
@@ -32,13 +33,12 @@ def scrapying_2(week):
         driver = webdriver.Chrome(options=options)
         #driver=webdriver.Chrome()
         driver.get("https://www.tdcc.com.tw/portal/zh/smWeb/qryStock")
-        logging.info("get stock code")
+        print("get stock code")
         gsc = get_stock_code.get_stock_code_data()
-        print(gsc)
         if len(gsc) == 0:
             break
         try :
-            for k in range(0,len(gsc)):  
+            for k in range(0,len(gsc)):
                 code = driver.find_element('xpath','/html/body/div[1]/div[1]/div/main/div[4]/form/table/tbody/tr[2]/td[2]/input')
                 code.clear()
                 code.send_keys(gsc[k])
@@ -57,18 +57,17 @@ def scrapying_2(week):
                     click1.click()
                     time.sleep(0.4)
                     temp = driver.find_elements('xpath',"/html/body/div[1]/div[1]/div/main/div[6]/div/table/tbody/*")
-                    print(len(temp))
                     if len(temp)==16:
                         for i in range(16):
                             temp_1 = driver.find_elements('xpath',"/html/body/div[1]/div[1]/div/main/div[6]/div/table/tbody/tr["+str(i+1)+"]/td[3]")
                             temp_2 = driver.find_elements('xpath',"/html/body/div[1]/div[1]/div/main/div[6]/div/table/tbody/tr["+str(i+1)+"]/td[4]")
                             if len(temp_1) != 0 and len(temp_2)!=0 :
                                 insert_data(clean_str(gsc[k]),clean_str(date_text),clean_str(i+1),clean_str(temp_1[0].text),clean_str(temp_2[0].text))
-                                logging.info(f"insert || stock : {clean_str(gsc[k])}, date : {clean_str(date_text)}, number : {clean_str(i+1)}")
+                                print(f"insert || stock : {clean_str(gsc[k])}, date : {clean_str(date_text)}, number : {clean_str(i+1)}")
                             else:
                                 temp_3 = driver.find_element('xpath','/html/body/div[1]/div[1]/div/main/div[6]/div/table/tbody/tr/td/span').text
                                 if temp_3 == '查無此資料':
-                                    logging.warning(f"no data || stock : {clean_str(gsc[k])}, date : {clean_str(date_text)}, number : {clean_str(i+1)}")
+                                    print(f"no data || stock : {clean_str(gsc[k])}, date : {clean_str(date_text)}, number : {clean_str(i+1)}")
                                     break
                     elif len(temp) == 17:
                         flag = 0
@@ -77,30 +76,30 @@ def scrapying_2(week):
                             temp_2 = driver.find_elements('xpath',"/html/body/div[1]/div[1]/div/main/div[6]/div/table/tbody/tr["+str(i+1)+"]/td[4]")
                             if len(temp_1) != 0 and len(temp_2)!=0 :
                                 insert_data(clean_str(gsc[k]),clean_str(date_text),clean_str(i+1),clean_str(temp_1[0].text),clean_str(temp_2[0].text))
-                                logging.info(f"insert || stock : {clean_str(gsc[k])}, date : {clean_str(date_text)}, number : {clean_str(i+1)}")
+                                print(f"insert || stock : {clean_str(gsc[k])}, date : {clean_str(date_text)}, number : {clean_str(i+1)}")
                             else:
                                 temp_3 = driver.find_element('xpath','/html/body/div[1]/div[1]/div/main/div[6]/div/table/tbody/tr/td/span').text
                                 if temp_3 == '查無此資料':
-                                    logging.warning(f"no data || stock : {clean_str(gsc[k])}, date : {clean_str(date_text)}, number : {clean_str(i+1)}")
+                                    print(f"no data || stock : {clean_str(gsc[k])}, date : {clean_str(date_text)}, number : {clean_str(i+1)}")
                                     flag = 1
                                     break
                         if flag == 0:
                             temp_1 = driver.find_elements('xpath',"/html/body/div[1]/div[1]/div/main/div[6]/div/table/tbody/tr[17]/td[3]")
                             temp_2 = driver.find_elements('xpath',"/html/body/div[1]/div[1]/div/main/div[6]/div/table/tbody/tr[17]/td[4]")
-                            logging.info(f"insert || stock : {clean_str(gsc[k])}, date : {clean_str(date_text)}, number : 16")
+                            print(f"insert || stock : {clean_str(gsc[k])}, date : {clean_str(date_text)}, number : 16")
                             insert_data(clean_str(gsc[k]),clean_str(date_text),"16",clean_str(temp_1[0].text),clean_str(temp_2[0].text))
                             temp_1 = driver.find_elements('xpath',"/html/body/div[1]/div[1]/div/main/div[6]/div/table/tbody/tr[16]/td[3]")
                             temp_2 = driver.find_elements('xpath',"/html/body/div[1]/div[1]/div/main/div[6]/div/table/tbody/tr[16]/td[4]")
-                            logging.info(f"insert || stock : {clean_str(gsc[k])}, date : {clean_str(date_text)}, number : 17")
+                            print(f"insert || stock : {clean_str(gsc[k])}, date : {clean_str(date_text)}, number : 17")
                             insert_data(clean_str(gsc[k]),clean_str(date_text),"17","nod",clean_str(temp_2[0].text))#Number of differences
-                logging.info(f"{gsc[k]} finish")
+                print(f"{gsc[k]} finish")
                 get_stock_code.set_stock_code(gsc[k])
-                
+
                 time.sleep(0.1)
         except Exception as e:
-            logging.error(f"error:{e}")
+            print(f"error:{e}")
             pass
-    logging.info("all finish")
+    print("all finish")
 
 
 def create_table():
@@ -110,27 +109,27 @@ def create_table():
     con.commit()
     con.close()
 
-    
+
 def insert_data(stock ,date ,number ,people ,share):
     #用?
     con = sqlite3.connect('database.db')
     cur = con.cursor()
-    """ print(f"INSERT INTO data(stock,date_time,number,people,share) VALUES" 
-                f"('{stock}'," 
+    """ print(f"INSERT INTO data(stock,date_time,number,people,share) VALUES"
+                f"('{stock}',"
                 f"'{date}',"
                 f"'{number}',"
                 f"'{people}',"
                 f"'{share}')") """
     print(f"{stock} {date} {number}")
     #logging.info(f"stock :{stock}, date :{date}, number :{number}")
-    cur.execute(f"INSERT INTO data(stock,date_time,number,people,share) SELECT " 
+    cur.execute(f"INSERT INTO data(stock,date_time,number,people,share) SELECT "
                 f"?,?,?,?,? "
                 f"WHERE NOT EXISTS"
                 f"(SELECT * FROM data WHERE "
                 f"stock=? and "
                 f"date_time=? and "
                 f"number=?)",(stock,date,number,people,share,stock,date,number,))
-    
+
     con.commit()
     con.close()
 
@@ -143,8 +142,7 @@ def get_data():
     con.commit()
     con.close()
 if __name__ == '__main__':
-    # get_data()
-    scrapying_2(37)
+    scrapying_2(2)
     # insert_data("0050","20240517","1","380744","103226454")
     # con.commit()
     # con.close()
